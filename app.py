4cbf177f8a2faaaf8813e9cbb0905cb8d4c2d35f
sarahmadhusudhan1504-cyber <sarahmadhusudhan1504@gmail.com>
2026-03-19 12:19:15 +0530

    Changes made

--- app.py
+++ app.py
@@ -3,38 +3,31 @@ import mysql.connector
 
 app = Flask(__name__)
 
-try:
-    db = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="A1HnZv16*",
-        database="portfolio"
-    )
-    cursor = db.cursor()
-except:
-    db = None
-    cursor = None
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="A1HnZv16*",
+    database="portfolio"
+)
 
+cursor = db.cursor()
 
 @app.route('/')
 def home():
     return render_template("index.html")
-
 
 @app.route('/submit', methods=['POST'])
 def submit():
     name = request.form['name']
     email = request.form['email']
 
-    if cursor:
-        sql = "INSERT INTO contacts (name,email) VALUES (%s,%s)"
-        val = (name,email)
+    sql = "INSERT INTO contacts (name,email) VALUES (%s,%s)"
+    val = (name, email)
 
-        cursor.execute(sql,val)
-        db.commit()
+    cursor.execute(sql, val)
+    db.commit()
 
     return "Data Stored Successfully"
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(debug=True)
